cf332eee9fe1a4d8bc828c64e789ada45cff1a18
Henry Guido Báez <henry_guido@hotmail.com>
2024-02-16 23:11:15 -0600

    add index router

---
+++ src/routes/IndexRouter.tsx
@@ -0,0 +1,39 @@
+// router
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+
+// pages
+import ErrorPage from '@/pages/ErrorPage'
+import Employees from '@/pages/Employess/Employees'
+import Layout from '@/layout/Layout'
+import Home from '@/pages/Home'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route element={<Layout />} errorElement={<ErrorPage />}>
+        <Route errorElement={<ErrorPage />}>
+          <Route
+            path={import.meta.env.VITE_ORIGIN_PATH_IIS}
+            element={<Home />}
+          />
+          <Route
+            path={`${import.meta.env.VITE_ORIGIN_PATH_IIS}/employees`}
+            element={<Employees />}
+          />
+        </Route>
+      </Route>
+      ,
+    </>,
+  ),
+)
+
+const IndexRouter = (): JSX.Element => {
+  return <RouterProvider router={router} />
+}
+
+export default IndexRouter
