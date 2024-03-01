fe1dfde7fcb81b181cd196fe408da431adc7ceed
Henry Guido Báez <henry_guido@hotmail.com>
2024-02-29 22:16:43 -0600

    add data dummy

---
+++ src/dummy/applicationdummy.ts
@@ -0,0 +1,16 @@
+import { type Application } from "@/types/applications";
+
+export const applicationDummy: Application[] = [
+  {
+    id: 1,
+    date: '2024-02-01',
+    deduced: 1,
+    status: "pendiente",
+  },
+  {
+    id: 2,
+    date: '2024-02-01',
+    deduced: 2,
+    status: "aprobada",
+  },
+]
