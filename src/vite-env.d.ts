6f0946b40bbaf897d384f5a00a539c421eb35ff5
Henry Guido Báez <henry_guido@hotmail.com>
2024-02-29 22:21:20 -0600

    add environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
-import 'react-scripts'
+interface ImportMetaEnv {
+  readonly VITE_MSAL_CLIENTID: string
+  readonly VITE_MSAL_AUTHORITY: string
+  readonly VITE_MSAL_REDIRECTURI: string
+  readonly VITE_ORIGIN_PATH_IIS: string
+  // more env variables...
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
