a7d1b83a7ffb483279b74caafa1ff9263c4b804d
Henry Guido Báez <henry_guido@hotmail.com>
2024-02-29 22:16:00 -0600

    add data table component ui

---
+++ src/components/DataColumns.tsx
@@ -0,0 +1,22 @@
+
+import { type Application } from "@/types/applications"
+import { type ColumnDef } from "@tanstack/react-table"
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+
+export const columns: Array<ColumnDef<Application>> = [
+  {
+    accessorKey: "date",
+    header : () => <div className="text-left">Fecha(s) de ausencia</div>,
+  },
+  {
+    accessorKey: "deduced",
+    header: () => <div className="text-left">Dias deducido</div>,
+    cell: ({row}) =><div className="ml-9">{row.getValue("deduced")}</div>
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+]
