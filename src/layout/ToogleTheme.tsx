1b77e027e3079f210cb8caff65680fc844076518
Scarleth Suarez <scarlethsuarez1995@gmail.com>
2024-03-03 16:36:46 -0600

    Feat: add timeout

--- src/layout/ToogleTheme.tsx
+++ src/layout/ToogleTheme.tsx
@@ -1,20 +1,57 @@
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import { useTheme } from "@/contexts/ThemeProvider";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
+import { useUserPreference } from "@/store/userPreference";
 
 
 
 const ToogleTheme = () : JSX.Element => {
-  const { theme ,setTheme } = useTheme()
+  const [theme, setTheme] = useUserPreference(state => [state.theme ,state.setTheme])
+  const [isChangingTheme, setIsChangingTheme] = useState(false);
+
+  useEffect(() => {
+    const setRootTheme = (color: string): void => {
+      const root = document.documentElement
+      root.classList.remove('light', 'dark')
+      root.classList.add(color);
+    };
+
+    // const handleThemeChange = (e: MediaQueryListEvent): void => {
+    //   setRootTheme(e.matches ? 'dark' : 'light')
+    // };
+
+    const handleThemeChange = (e: MediaQueryListEvent): void => {
+      setIsChangingTheme(true);
+      setTimeout(() => {
+        setRootTheme(e.matches ? 'dark' : 'light');
+        setIsChangingTheme(false);
+      }, 300); // Tiempo de espera en milisegundos, ajusta según la duración de tu animación CSS
+    };
+
+    const themeSystem = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if (theme === 'system') {
+      themeSystem.addEventListener('change', handleThemeChange)
+      setRootTheme(themeSystem.matches ? 'dark' : 'light')
+    } else {
+      themeSystem.removeEventListener('change', handleThemeChange)
+      setRootTheme(theme);
+    }
+
+    return () => {
+      themeSystem.removeEventListener('change', handleThemeChange)
+    };
+
+  }, [theme]);
 
   return (
     <div className="ml-auto" >
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button className="focus-visible:ring-transparent" variant="ghost" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <SunIcon className={`h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all ${isChangingTheme ? '' : 'dark:-rotate-90 dark:scale-0'}`} />
+            <MoonIcon className={`absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all ${isChangingTheme ? '' : 'dark:rotate-0 dark:scale-100'}`} />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
